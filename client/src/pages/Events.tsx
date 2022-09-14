acab83556689e888973c81161eec12d326cd4661
Adam Johnson <adam@adamjohnson.org>
2022-09-14 19:28:35 +1000

    Events

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -3,7 +3,7 @@ import './events.css';
 
 /* Event */
 
-function Event(props: { theme: string; dateType: string; date: string; }) {
+function Event(props: { theme: string; dateType: string; date: string; image: string; bgColor: string; parent: string }) {
   const navigate = useNavigate();
 
   const ctaClick = (location: String) => {
@@ -11,18 +11,22 @@ function Event(props: { theme: string; dateType: string; date: string; }) {
   }
 
   return (
-    <div className="event">
+    <div className="event" style={{ opacity: props.date === 'Soon' || props.parent === 'Vote' ? 0.5 : 1 }}>
       <div className="eventInfo">
         <span>Theme</span>
         <h3>{ props.theme }</h3>
         <span>{ props.dateType }</span>
         <h4>{ props.date }</h4>
         <span>Prizes</span>
-        <button onClick={ () => ctaClick('wardrobe') } className="bigButton" style={{ backgroundColor: props.dateType === 'Starts' ? 'var(--df-green)' : props.dateType === 'Ends' ? 'var(--df-orange)' : 'var(--df-red)' }}>Wardrobe</button>
+        <div className="infoPrize" style={{ backgroundColor: props.bgColor }}>?</div>
+        <div className="infoPrize" style={{ backgroundColor: props.bgColor }}>?</div>
+        <div className="infoPrize" style={{ backgroundColor: props.bgColor }}>?</div>
+        <div className="clear"></div>
+        <button onClick={ () => ctaClick('wardrobe') } className="bigButton" style={{ backgroundColor: props.dateType === 'Starts' && props.parent !== 'Vote' ? 'var(--df-green)' : props.dateType === 'Ends' || props.parent === 'Vote' ? 'var(--df-orange)' : 'var(--df-red)', pointerEvents: props.date === 'Soon' || props.parent === 'Vote' ? "none" : "auto" }}>{ props.parent === 'Vote' ? 'Vote' : 'Wardrobe' }</button>
       </div>
 
-      <div className="eventImage">
-        <img src="assets/img/home-darcel-2.svg" alt="" />
+      <div className="eventImage" style={{ backgroundColor: props.bgColor }}>
+        <img src={ 'assets/img/events/' + props.image } alt="" />
       </div>
     </div>
   )
@@ -36,7 +40,11 @@ function EventGroup(props: { title: string; bgColor: string }) {
       <h2>{ props.title }</h2>
 
       <div className="groupEvents">
-        <Event theme="Slam Jam" dateType="Starts" date="September 22" />
+        <Event theme="Slam Jam" dateType="Starts" date="September 22" image="slam-jam.png" bgColor="var(--df-deep-red)" parent={ props.title } />
+
+        { props.title === 'Upcoming' &&
+        <Event theme="TBA" dateType="Starts" date="Soon" image="tba.png" bgColor="var(--df-deep-yellow)" parent={ props.title } />
+        }
       </div>
 
       <div className="clear"></div>
@@ -50,6 +58,7 @@ function Events() {
   return (
     <div className="section" id="sectionEvents">
       <EventGroup title="Upcoming" bgColor="var(--df-pink)" />
+      <EventGroup title="Vote" bgColor="var(--df-blue)" />
     </div>
   )
 }
