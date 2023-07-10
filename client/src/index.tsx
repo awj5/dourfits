825334c2847918f09eccfca2915bdb52cc84ab01
Adam Johnson <adam@adamjohnson.org>
2023-07-10 21:49:12 +1000

    Update Wagmi

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,27 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { WagmiConfig, createClient, defaultChains, configureChains } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
-import Layout from './Layout';
-import Home from './pages/Home';
-import Wardrobe from './pages/Wardrobe';
-import Events from './pages/Events';
-import FAQ from './pages/FAQ';
-import Vote from './pages/Vote';
-import Results from './pages/Results';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { WagmiConfig, createClient, configureChains } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { mainnet } from "wagmi/chains";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Wardrobe from "./pages/Wardrobe";
+import Events from "./pages/Events";
+import FAQ from "./pages/FAQ";
+import Vote from "./pages/Vote";
+import Results from "./pages/Results";
+import "./index.css";
 
-window.Buffer = require('buffer').Buffer; // Polyfill to fix buffer not defined in WAGMI
+window.Buffer = require("buffer").Buffer; // Polyfill to fix buffer not defined in WAGMI
 
-const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
-  alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID }),
-  publicProvider(),
-]);
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID! }), publicProvider()]
+);
 
 const client = createClient({
   autoConnect: true,
@@ -29,32 +30,32 @@ const client = createClient({
     new WalletConnectConnector({
       chains,
       options: {
-        qrcode: true,
+        projectId: "7300e1865c6b102d18b0c0b8d727f5c1",
       },
     }),
     new CoinbaseWalletConnector({
       chains,
       options: {
-        appName: 'wagmi',
+        appName: "wagmi",
       },
-    })
+    }),
   ],
   provider,
-  webSocketProvider
+  webSocketProvider,
 });
 
 export default function App() {
   return (
-    <WagmiConfig client={ client }>
+    <WagmiConfig client={client}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <Layout /> }>
-            <Route index element={ <Home /> } />
-            <Route path="wardrobe" element={ <Wardrobe /> } />
-            <Route path="events" element={ <Events /> } />
-            <Route path="faq" element={ <FAQ /> } />
-            <Route path="vote/:id" element={ <Vote /> } />
-            <Route path="results/:id" element={ <Results /> } />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="wardrobe" element={<Wardrobe />} />
+            <Route path="events" element={<Events />} />
+            <Route path="faq" element={<FAQ />} />
+            <Route path="vote/:id" element={<Vote />} />
+            <Route path="results/:id" element={<Results />} />
           </Route>
         </Routes>
       </BrowserRouter>
@@ -62,5 +63,5 @@ export default function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
